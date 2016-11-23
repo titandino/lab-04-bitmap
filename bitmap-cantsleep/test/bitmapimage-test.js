4e4cc7e68a917041b4633fb81a57978060a0e0d7
Trenton Kress <trenton.kress@gmail.com>
2016-11-23 04:24:16 -0800

    Made a test for the load method.

--- bitmap-cantsleep/test/bitmapimage-test.js
+++ bitmap-cantsleep/test/bitmapimage-test.js
@@ -1,10 +1,20 @@
 'use strict';
 
-const BitMapImage = require('../lib/bitmapimage');
+const BMP = require('../lib/bitmapimage');
 const assert = require('assert');
 
-describe('BMPTrans', function() {
-  describe('#invert()', function() {
+let bmp = new BMP();
 
+describe('BitmapImage', function() {
+  describe('#load()', function() {
+    it('should read the bitmap data and assign it to the bitmap object', function(done) {
+      bmp.load('./test/test.bmp', function() {
+        if (bmp.data) {
+          done();
+        } else {
+          done(new Error('No bitmap data detected.'));
+        }
+      });
+    });
   });
 });
