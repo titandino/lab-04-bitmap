c33970be94b4afb2e9f0bbb02e57866113bffb21
Trenton Kress <trenton.kress@gmail.com>
2016-11-23 01:24:00 -0800

    Scaffolded test file real fast.

--- bitmap-cantsleep/test/bmptrans-test.js
+++ bitmap-cantsleep/test/bmptrans-test.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const bmp = require('../lib/bmptrans');
+const assert = require('assert');
+
+describe('BMPTrans', function() {
+  describe('#invert()', function() {
+    
+  });
+});
